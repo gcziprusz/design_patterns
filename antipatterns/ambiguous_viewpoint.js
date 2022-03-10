1fb6e2e84f4dd0aee5579a4348e5213b0cbf64da
Gabriel Cziprusz <gcziprusz@gmail.com>
2022-03-10 06:31:33 -0600

    Create ambiguous_viewpoint.js

---
+++ antipatterns/ambiguous_viewpoint.js
@@ -0,0 +1,11 @@
+// https://sourcemaking.com/antipatterns/ambiguous-viewpoint
+
+// Ambiguous Viewpoint antipatterns is present is the model(s) do not express the viewpoint they represent. 
+
+// The refactored example of this antipattern is a telephone exchange systems 
+// design that significantly differs based on the perspective:
+
+// - end user that cares about the ease of making calls, 
+// - operator concerned about connecting phone numbers 
+// - billing dept concerned with log of calls and formula for calculating charges
+
